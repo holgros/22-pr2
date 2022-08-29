1313e0c54067ccf7f1784eb00824a2d95a88967f
Elev <holgros@gmail.com>
2022-08-29 14:22:42 +0200

    Mer om elementära datatyper

---
+++ 1-1 elementära datatyper/ovrigaelementaradatatyper.py
@@ -0,0 +1,42 @@
+# strängar och tupplar
+s = "Hej på dig!"
+print(len(s))   # 11
+print(s[0])     # "H"
+print(s[10])    # "!"
+print(s.upper())    # inbyggd metod till sträng, gör om till stora bokstäver
+tuple = s.partition("på")   # skapar en tippel bestående av tre element
+print(tuple)    # ("Hej", "på", "dig!")
+#tuple[2] = "mig!"   # FEL! Tipplar kan INTE ändras när de väl har skapats!
+
+# en dictionary (associativt fält) förknipar varje nyckel med ett värde
+dic = {"Sverige": "Stockholm", "Norge": "Oslo", "Finland": "Helsingfors"}
+print(dic["Sverige"])               # "Stockholm"
+print(dic["Norge"])                 # "Oslo"
+print(dic["Finland"])               # "Helsingfors"
+#print(dic["Danmark"])               # KeyError
+# metoder för associativa fält
+dic.update({"Estland": "Tallinn"})	# lägga till ett nyckel-värdepar
+dic.pop("Sverige")			# ta bort nyckel-värdeparet med nyckeln "Sverige"
+
+# nycklar behöver inte vara strängar
+dic = {1: "Foo", "Bar": "Baz", True: False} # OK!
+
+# booleska variabler
+bool = True or 0
+print(bool)         # True
+bool = True and 1
+print(bool)         # 1
+bool = not False or ""
+print(bool)         # True
+bool = False and ""
+print(bool)         # False
+bool = True and ""
+print(bool)         # tom sträng
+
+# mängd (set)
+s = set()   # anropa konstruktorn
+s.add("Äpple")
+s.add("Banan")
+s.add("Päron")
+s.add("Äpple")      # Vi har redan ett äpple
+print(s)            # {"Banan", "Päron", "Äpple"}
