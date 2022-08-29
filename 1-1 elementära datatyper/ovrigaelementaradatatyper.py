d2ebeaa26638adfe9f6dc100e45e79846f9c6f5d
Elev <holgros@gmail.com>
2022-08-29 14:32:38 +0200

    Mer om elementära datatyper

--- 1-1 elementära datatyper/ovrigaelementaradatatyper.py
+++ 1-1 elementära datatyper/ovrigaelementaradatatyper.py
@@ -3,20 +3,23 @@ s = "Hej på dig!"
 print(len(s))   # 11
 print(s[0])     # "H"
 print(s[10])    # "!"
-print(s.upper())    # inbyggd metod till sträng, gör om till stora bokstäver
+print(s.upper())    # inbyggd metod till sträng, returnerar en sträng med stora bokstäver (men ändrar inte den ursprungliga textsträngen)
+print("Hej på dig!"[2]) # "j"
 tuple = s.partition("på")   # skapar en tippel bestående av tre element
-print(tuple)    # ("Hej", "på", "dig!")
-#tuple[2] = "mig!"   # FEL! Tipplar kan INTE ändras när de väl har skapats!
+print(tuple)        # ("Hej", "på", "dig!")
+array = ["Hej", "på", "dig!"]   # skapar ett fält liknande ovanstående tippel
+array[2] = "mig!"   # OK! Fält kan ändras.
+#tuple[2] = "mig!"  # FEL! Tipplar kan INTE ändras när de väl har skapats!
 
 # en dictionary (associativt fält) förknipar varje nyckel med ett värde
 dic = {"Sverige": "Stockholm", "Norge": "Oslo", "Finland": "Helsingfors"}
 print(dic["Sverige"])               # "Stockholm"
 print(dic["Norge"])                 # "Oslo"
 print(dic["Finland"])               # "Helsingfors"
-#print(dic["Danmark"])               # KeyError
+#print(dic["Danmark"])              # KeyError
 # metoder för associativa fält
 dic.update({"Estland": "Tallinn"})	# lägga till ett nyckel-värdepar
-dic.pop("Sverige")			# ta bort nyckel-värdeparet med nyckeln "Sverige"
+dic.pop("Sverige")			        # ta bort nyckel-värdeparet med nyckeln "Sverige"
 
 # nycklar behöver inte vara strängar
 dic = {1: "Foo", "Bar": "Baz", True: False} # OK!
